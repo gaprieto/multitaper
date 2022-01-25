46e207cfb123577c6918c0e7a0e65c575b7697b6
gaprieto <gaprietogo@unal.edu.co>
2022-01-25 10:20:08 -0500

    test1

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="multitaper",
-    version="1.0.2",
+    version="1.0.3",
     author="German A. Prieto",
     author_email="gaprietogo@unal.edu.co",
     description="Multitaper codes translated into Python",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gaprieto/multitaper",
     classifiers=[
@@ -17,6 +13,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=find_packages(),
-    package_data={'multitaper':['data/*.dat']}
-    python_requires=">=3.7",
-)
+    python_requires=">=3.8",
+    data_files=[('data',['data/ADO.dat','data/PASC.dat','data/asc_akima.dat'])],
+)   
