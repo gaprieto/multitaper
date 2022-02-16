472b429047bd2678ec99eafe1567cc02e559f451
gaprieto <gaprietogo@unal.edu.co>
2022-02-16 10:30:48 -0500

    v1.1.0 sphinx

--- docs/source/conf_local.py
+++ docs/source/conf_local.py
@@ -13,9 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-#sys.path.insert(0, os.path.abspath('../../multitaper/'))
+sys.path.insert(0, os.path.abspath('../../multitaper/'))
 print('New path ', sys.path)
 
 # -- Project information -----------------------------------------------------
